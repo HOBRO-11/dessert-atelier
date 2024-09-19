c58d1c584ea7b7af096a59c170821d90b3bdf97c
Hobro <hobro3710@naver.com>
2024-09-19 17:12:09 +0900

    init proj

---
+++ backend/dessert_atelier/src/main/java/com/yangnjo/dessert_atelier/DessertAtelierApplication.java
@@ -0,0 +1,13 @@
+package com.yangnjo.dessert_atelier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessertAtelierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DessertAtelierApplication.class, args);
+	}
+
+}
---
+++ backend/dessert_atelier/src/test/java/com/yangnjo/dessert_atelier/DessertAtelierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangnjo.dessert_atelier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DessertAtelierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
