f4d65b1730092d5c372d242224920e9cd951fe78
Hobro <hobro3710@naver.com>
2024-12-31 21:02:20 +0900

    modified display-product form

--- backend/dessert_atelier/src/main/java/com/yangnjo/dessert_atelier/service/product/dto/DisplayProductEntityCreateForm.java
+++ backend/dessert_atelier/src/main/java/com/yangnjo/dessert_atelier/service/product/dto/DisplayProductEntityCreateForm.java
@@ -2,18 +2,24 @@ package com.yangnjo.dessert_atelier.service.product.dto;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.yangnjo.dessert_atelier.domain_service.product.dto.DisplayProductCreateDto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class DisplayProductEntityCreateForm {
     String title;
     List<String> thumb;
-    String desc;
+    List<String> desc;
     Integer optionLayer;
+    List<MultipartFile> thumbImages;
+    List<MultipartFile> descImages;
 
-    public DisplayProductCreateDto toDto(String path) {
+    public DisplayProductCreateDto toDto(List<String> thumb, List<String> desc) {
         return new DisplayProductCreateDto(title, thumb, desc, optionLayer);
     }
 }
